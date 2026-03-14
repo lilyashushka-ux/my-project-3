560821bda5d6bfb969efdbdb20f63fac415bf6d0
Liliya <lilyashushka@gmail.com>
2026-03-14 21:26:22 +0200

    upd index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-export default function App() {
+const App = () => {
   return (
-    <h1 className="text-5xl text-red-500 font-bold">
-      Tailwind працює 🚀
-    </h1>
+    <div className="min-h-screen overflow-x-hidden bg-background">
+
+    </div>
   );
 }
-
+export default App;
